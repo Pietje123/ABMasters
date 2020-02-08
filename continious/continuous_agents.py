d3d6beaf7e8de37173cadf030bc374f40f89b458
ardanwan <jozsef68@gmail.com>
2020-02-08 17:53:01 +0100

    Cleanup

--- continious/continuous_agents.py
+++ continious/continuous_agents.py
@@ -1,9 +1,19 @@
-from mesa import Agent
 import numpy as np
+from mesa import Agent
+
 from continuous_dijkstra import dijkstra
 from continuous_forces import totalforce
 
 class Node:
+    """
+    Node for the discrete grid used for pathfinding with Dijkstra's algorithm.
+
+    Attributes:
+    self.pos : position in grid (tuple)
+    self.done : check whether this node should be considered (Boolean)
+    self.exit : check whether node is an exit (Boolean)
+    self.path : current shortest path to node
+    """
     def __init__(self, position):
         self.pos = position
         self.done = False
@@ -12,9 +22,12 @@ class Node:
 
     def __str__(self):
         x, y = self.pos
-        return f"{x / 3, y / 3}"
+        return f"{x / 6, y / 6}"
 
 class Objects(Agent):
+    """
+    Parent object for all agents (inherits from Mesa Agent).
+    """
     def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
@@ -36,42 +49,65 @@ class Exit(Objects):
         super().__init__(model, pos)
 
 class Human(Objects):
+    """
+    Human agents in simulation.
+
+    Attributes:
+    self.new_pos : specified movement target (tuple)
+    self.node : closest node in grid
+    self.path : current shortest path to exit
+    self.dist : lenght of current shortest path
+    """
     def __init__(self, model, pos):
         super().__init__(model, pos)
         getattr(model, f'scheduler').add(self)
         self.new_pos = 0
-        self.path = []
-        self.node = 3 * self.pos[0], 3 * self.pos[1]
+        self.node = 6 * self.pos[0], 6 * self.pos[1]
+        self.path = dijkstra(self.model.grid, self.node)
+        self.dist = len(self.path)
 
     def dijkstra(self):
+        """
+        Pathfinding algorithm, calculates path & distance.
+        """
         self.path = dijkstra(self.model.grid, self.node)
+        self.dist = len(self.path)
 
     def force(self):
+        """
+        Calculates forces from Social Force Model (currently only self-driving
+        force and human-human interactions).
+        """
         self.new_pos = totalforce(self.pos, self.path[0], self.model.humans)
-        print(f"Pos: {self.pos}, Target: {self.new_pos}")
-        print(f"Node: {self.node}, Path: {self.path}")
 
     def get_node(self):
+        """
+        Finds closest node in grid.
+        """
         xs, ys = self.pos
-        self.node = int(np.round(xs * 3)), int(np.round(ys * 3))
-        # dist = np.sqrt((xe / 3 - xs)**2 + (ye / 3 - ys)**2)
-        # if dist < 0.16666 and len(self.path) != 0:
-        #     self.node = self.path[0]
+        self.node = int(np.round(xs * 6)), int(np.round(ys * 6))
 
     def move(self):
-        try:
-            self.model.space.move_agent(self, self.new_pos)
-        except:
-            pass
+        """
+        Moves agent in Mesa ContinuousSpace.
+        """
+        self.model.space.move_agent(self, self.new_pos)
 
     def step(self):
+        """
+        Executes all methods for 1 simulation step in the right order.
+        Currently uses try/Except as failsafe.
+        """
         try:
             self.dijkstra()
             self.get_node()
             self.force()
             self.move()
         except Exception as e:
-            pass
+            print(e)
 
     def saved(self):
+        """
+        Removes agent from Mesa class ContinuousSpace.
+        """
         self.model.remove_agent(self)
--- continious/continuous_scheduler.py
+++ continious/continuous_scheduler.py
@@ -2,6 +2,10 @@ import numpy as np
 import random as rnd
 
 class DistanceScheduler:
+    """
+    Scheduler that activates agents in order of path-to-exit distance. Closest
+    agents are activated first to maximize flow through exit.
+    """
     def __init__(self, model):
         self.steps = 0
         self.agents = []
@@ -15,18 +19,29 @@ class DistanceScheduler:
             self.agents.remove(agent)
 
     def step(self):
-        counter = 1
+        """
+        Sorts agents in order of distance to exit and then steps
+        """
+        try:
+            self.agents.sort(key=lambda x: x.dist)
+        except Exception as e:
+            print(e)
+
         for agent in self.agents:
-            agent.step()
-            print(f"Agent {counter} of {len(self.agents)}")
-            counter += 1
+            try:
+                agent.step()
+            except Exception as e:
+                print(e)
+
+
+            # Removes agents if they reach exit
             for exit in self.model.exits:
-                x, y = exit.pos[0] * 3 + 1, exit.pos[1] * 3 + 1
+                x, y = exit.pos[0] * 6 + 1, exit.pos[1] * 6 + 1
                 if agent.node == (x, y):
                     try:
                         agent.saved()
-                    except Exception:
-                        pass
+                    except Exception as e:
+                        print(e)
 
     def get_agent_count(self):
         return len(self.agents)
